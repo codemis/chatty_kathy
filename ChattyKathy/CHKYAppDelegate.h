6b1e02aefa9f56fef93be2bbfed03b893c1a46cc
Codemis <johnathan@missionaldigerati.com>
2013-04-07 17:18:15 -0700

    Initial Commit

---
+++ ChattyKathy/CHKYAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  CHKYAppDelegate.h
+//  ChattyKathy
+//
+//  Created by Johnathan Pulos on 4/7/13.
+//  Copyright (c) 2013 Johnathan Pulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CHKYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ ChattyKathy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChattyKathy
+//
+//  Created by Johnathan Pulos on 4/7/13.
+//  Copyright (c) 2013 Johnathan Pulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CHKYAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CHKYAppDelegate class]));
+    }
+}
